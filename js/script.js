e4ed80556e19baa2b5be23e8af8d7ce0ef7e1dc2
Angel Zacarias <angelqz2001@gmail.com>
2025-01-23 17:54:34 -0500

    version 2

--- js/script.js
+++ js/script.js
@@ -1,43 +1,117 @@
 import validacion from '../js/validaciones.js';
 
 const btnLogin = document.getElementById('btnLogin');
+const btnRegister = document.getElementById('btnRegister');
 
-btnLogin.addEventListener('click', () => {
+btnRegister.addEventListener('click', () => {
 
-    const ValEmail = document.getElementById('txtEmail').value;
-    const ValContraseña = document.getElementById('txtContraseña').value;
+    const username = document.getElementById('txtEmail').value;
+    const password = document.getElementById('txtContraseña').value;
 
     let ArrayValue = []
 
     // console.log(ValEmail + ' ' + ValContraseña)
-    if (!validacion.ValidarEmail(ValEmail)) {
+    if (!validacion.ValidarEmail(username)) {
         throw Error("Hubo un error al validar Email");
-    }    
+    }
 
-    if (!validacion.ValidarPassword(ValContraseña)) {
+    if (!validacion.ValidarPassword(password)) {
         throw Error("Hubo un error al validar la contraseña");
     }
 
     ArrayValue = {
-        ValEmail,
-        ValContraseña
+        username,
+        password
     }
 
     ArrayValue = JSON.stringify(ArrayValue);
 
     /*console.log(ArrayValue);*/
 
-    fetch('/registration', {
-        method: 'POST',
-        headers: {
-            "Conten-Type": "application/json"
-        },
-        body: ArrayValue
-    })
-    .then((data) => {
-        console.log(data);
-    })
-    .catch((e) => {
-        console.log(e.Message);
-    });
+    const registerUser = async () => {
+
+        try {
+
+            const response = await fetch('http://localhost:4000/api/users/register', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: ArrayValue
+            });
+
+            if (!response) {
+                throw Error("Hubo un error al hacer el registro");
+            }
+
+            const result = await response.json();
+
+            console.log(result);
+            return result;
+
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    }
+
+    registerUser();
+
+});
+
+
+btnLogin.addEventListener('click', () => {
+
+    const username = document.getElementById('txtEmail').value;
+    const password = document.getElementById('txtContraseña').value;
+
+    let ArrayValue = []
+
+    // console.log(ValEmail + ' ' + ValContraseña)
+    if (!validacion.ValidarEmail(username)) {
+        throw Error("Hubo un error al validar Email");
+    }    
+
+    if (!validacion.ValidarPassword(password)) {
+        throw Error("Hubo un error al validar la contraseña");
+    }
+
+    ArrayValue = {
+        username,
+        password
+    }
+
+    ArrayValue = JSON.stringify(ArrayValue);
+
+    /*console.log(ArrayValue);*/
+
+    const LoginUser = async () => {
+        
+        try {
+            
+            const response = await fetch('http://localhost:4000/api/users/login', {
+                method: 'POST',
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: ArrayValue
+            });
+
+            if (!response) {
+                throw Error("Hubo un error al Iniciar Session");
+            }
+
+            const result = await response.json();
+
+            console.log(result);
+            return result;
+
+        } catch (e) {
+            console.log(e.message);
+        }
+
+    }
+
+    LoginUser();
+    
 });
